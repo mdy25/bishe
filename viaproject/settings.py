f2b2647a1f067f42d2f4da519cdd0c1bdbfd750e
mdy25 <876795712@qq.com>
2020-04-22 18:02:16 +0800

    setting

--- viaproject/settings.py
+++ viaproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-i#qr8#@i%vtf(-u7-_fncnt#b4+0i5acgcor#0tcyvi!@l8x#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.martin-via.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
