2a06a842d092837ecaf659287379af663ff92602
mdy25 <876795712@qq.com>
2020-04-22 16:11:27 +0800

    via

---
+++ via/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+ 
+ 
+def index(request):
+    return render(request, 'via/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
