36b611aba916062a91ca49875e0185f9ee22daca
mdy25 <876795712@qq.com>
2020-04-22 23:37:42 +0800

    setting

--- viaproject/settings.py
+++ viaproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-i#qr8#@i%vtf(-u7-_fncnt#b4+0i5acgcor#0tcyvi!@l8x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ','.martin-via.cn']
 
